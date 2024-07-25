54b8be9dd5c6e1815d8a07fd3a0a812f484b5703
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2024-07-25 01:44:25 -0600

    feat(camera-move):se añadio un script para mover la cámara

---
+++ Ghost/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+public class CameraMove : MonoBehaviour
+{
+    bool IS_DPRESSED;
+    bool IS_APRESSED;
+    public float moveSpeed = 5f; // Velocidad de movimiento de la cámara
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IS_APRESSED = false;
+        IS_DPRESSED = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) IS_DPRESSED = true;
+        if (Input.GetKeyDown(KeyCode.A)) IS_APRESSED = true;
+
+        if (Input.GetKeyUp(KeyCode.D)) IS_DPRESSED = false;
+        if (Input.GetKeyUp(KeyCode.A)) IS_APRESSED = false;
+
+        if (IS_DPRESSED) gameObject.transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        if (IS_APRESSED) gameObject.transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+}
